fce86101f8d6c4c1c69c2404dd990d03f179ee1d
nt-squared <ngoctamnguyen3147@gmail.com>
2023-01-28 04:39:21 +0700

    add all files weather and air quality app

---
+++ js/air_quality.js
@@ -0,0 +1,59 @@
+export const airContent = [
+    {
+        index: 1,
+        icon: 'bi-shield-check',
+        category: 'Good',
+        color: '#acd06b',
+        recommendation:
+            'Air quality is satisfactory and poses little or no risk.'
+    },
+    {
+        index: 2,
+        icon: 'bi-shield-exclamation',
+        category: 'Moderate',
+        color: '#f7d26b',
+        recommendation:
+            'Sensitive individuals should avoid outdoor activity'
+            + ' as they may experience respiratory symptoms.'
+    },
+    {
+        index: 3,
+        icon: 'bi-shield-exclamation',
+        category: 'Unhealthy for sensitive groups',
+        color: '#fb985d',
+        recommendation:
+            'General public and sensitive individuals in particular are at risk'
+            + ' to experience irritation and respiratory problems.'
+    },
+    {
+        index: 4,
+        icon: 'bi-shield-x',
+        category: 'Unhealthy',
+        color: '#f5666c',
+        recommendation:
+            'Increase likelihood or adverse effects and aggravation'
+            + ' to the heart and lungs among general public.'
+    },
+    {
+        index: 5,
+        icon: 'bi-shield-x',
+        category: 'Very Unhealthy',
+        color: '#a27eb6',
+        recommendation:
+            'General public will be noticeably affected.'
+            + " Sensitive groups should restrict outdoor activities."
+    }
+]
+
+export function getAirQuality(lat, lon) {
+    return fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=f3a5cef19c155a0860cd1bcbcc3e3977`)
+        .then(async (res) => await res.json())
+        .then(data => {
+            console.log(data);
+            const { list: [{ components, main }] } = data;
+            return {
+                airIndex: main.aqi,
+                pm25: components.pm2_5
+            }
+        })
+}
---
+++ js/index.js
@@ -0,0 +1,113 @@
+import { getAirQuality, airContent } from "./air_quality.js"
+
+const searchInput = document.querySelector('#search-input');
+const DEFAULT_VALUE = '--';
+
+const API_ID = 'f3a5cef19c155a0860cd1bcbcc3e3977';
+
+let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric';
+
+// https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid={API key}&units=metric
+
+
+// Get current position and render weather
+navigator.geolocation.getCurrentPosition(success, error);
+
+function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_ID}&units=metric`)
+        .then(async (res) => await res.json())
+        .then(data => {
+            renderWeather(data);
+        })
+        .catch(error => {
+            console.error(error);
+            alert("Error getting weather.")
+        });
+
+    getAirQuality(latitude, longitude)
+        .then(renderAirQuality)
+        .catch(error => {
+            console.error(error);
+            alert("Error getting air quality index.")
+        });
+}
+
+function error() {
+    console.log('Unable to retrieve your location');
+}
+
+// helper function
+function setValue(selector, value = DEFAULT_VALUE) {
+    document.querySelector(`.${selector}`).textContent = value;
+}
+
+// render weather
+function renderWeather(data) {
+    console.log(data);
+    renderMainInfo(data);
+    renderAdditionalInfo(data);
+    document.body.classList.remove('blurred');
+}
+
+// render main information
+function renderMainInfo({ main, name, weather }) {
+    [weather] = weather;
+    setValue('city-name', name);
+    setValue('weather-state', weather.description);
+    setValue('current-temp', Math.round(main.temp));
+    document.querySelector('.weather-icon').src = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`
+}
+
+// render additional information
+const HOUR_FORMATTER = new Intl.DateTimeFormat('default', { hour: "numeric", minute: "2-digit" });
+
+function renderAdditionalInfo({ main, sys, wind }) {
+    setValue('sunrise', HOUR_FORMATTER.format(sys.sunrise * 1000));
+    setValue('sunset', HOUR_FORMATTER.format(sys.sunset * 1000));
+    setValue('humidity', main.humidity);
+    setValue('wind', (wind.speed * 3.6).toFixed(2));
+}
+
+// render air quality
+function renderAirQuality({ airIndex, pm25 }) {
+    airContent.forEach(airItem => {
+        if (airItem.index === airIndex) {
+            document.querySelector('.air-details').style.backgroundColor = airItem.color;
+            document.querySelector('.air-icon i').classList.add(`${airItem.icon}`);
+            setValue('air-category', airItem.category)
+            setValue('air-figure', Math.round(pm25))
+            setValue('air-recommendation', airItem.recommendation)
+        }
+    })
+}
+
+// render weather when searching city
+searchInput.addEventListener('change', (event) => {
+    let cityName = event.target.value;
+
+    if (cityName == null || cityName == "" || (!(isNaN(cityName)))) {
+        alert('Please enter city name.');
+        event.target.value = "";
+        return
+    }
+    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    fetch(url + `&q=${cityName}&appid=${API_ID}`)
+        .then(async (res) => {
+            const data = await res.json();
+            renderWeather(data);
+
+            const { coord: { lat, lon } } = data;
+            getAirQuality(lat, lon)
+                .then(renderAirQuality)
+                .catch(error => {
+                    console.error(error);
+                    alert("Error getting air quality index.")
+                });
+        })
+        .catch(error => {
+            console.error(error);
+            alert("Error getting weather.")
+        });
+});
